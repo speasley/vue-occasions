61a19d631942babcaceb89b3186ddf6ff5d7ac63
speasley <s@stephenpeasley.com>
2023-04-03 00:51:43 -0600

    feat: refactor weekdayBefore params substr

--- src/plugins/VueOccasions/core/specialDate.js
+++ src/plugins/VueOccasions/core/specialDate.js
@@ -1,17 +1,21 @@
+import { nthDay } from './nthDay'
+import { lastWeekday, weekdayBefore } from './weekday'
+
 const specialDate = (date, override) => {
+
   let params;
   let new_date;
 
   if (date.startsWith('_nthDay')) {
-    params = date.substring(7, date.length - 1);
+    params = date.substring(8, date.length - 1);
     new_date = nthDay(params, override);
   } else if (date.startsWith('_weekda')) {
-    params = date.substring(14, date.length - 1);
+    params = date.substring(15, date.length - 1);
     new_date = weekdayBefore(params, override);
-  } else if (date.startsWith('_lastWe')) {
+  } /*else if (date.startsWith('_lastWe')) {
     params = date.substring(12, date.length - 1);
     new_date = lastWeekday(params, override);
-  }
+  }*/
 
   return new_date;
 };
