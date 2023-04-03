cd2792e73da7307b677cca38a71716147613018f
speasley <s@stephenpeasley.com>
2023-04-03 11:00:25 -0600

    chore: remove mergeHashes method

--- src/plugins/VueOccasions/core/index.js
+++ src/plugins/VueOccasions/core/index.js
@@ -1,5 +1,4 @@
 import { globalYear } from "./globalYear";
-import { mergeHashes } from "./mergeHashes";
 import { monthIndex, monthName } from "./month"
 import { nthDay } from "./nthDay";
 import { renameKey } from "./renameKey";
@@ -7,6 +6,6 @@ import { specialDate } from "./specialDate";
 import { timestamp } from "./timestamp";
 import { todaysDate } from "./todaysDate";
 import { getPath, sanitizePath } from "./utils"
-import { lastWeekday, weekdayBefore, weekdayIndex, weekdayName } from "./weekday"
+import { firstWeekday, lastWeekday, weekdayBefore, weekdayIndex, weekdayName } from "./weekday"
 
-export { globalYear, mergeHashes, monthIndex, monthName, nthDay, renameKey, specialDate, timestamp, todaysDate, getPath, sanitizePath, lastWeekday, weekdayBefore, weekdayIndex, weekdayName }
+export { globalYear, monthIndex, monthName, nthDay, renameKey, specialDate, timestamp, todaysDate, getPath, sanitizePath, firstWeekday, lastWeekday, weekdayBefore, weekdayIndex, weekdayName }
--- src/plugins/VueOccasions/core/mergeHashes.js
+++
@@ -1,9 +0,0 @@
-const mergeHashes = (obj, src) => {
-  Object.keys(src).forEach(key => {
-    obj[key] = src[key];
-  });
-
-  return obj;
-};
-
-export { mergeHashes }
