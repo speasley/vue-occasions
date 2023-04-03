92bb06c2416063a4d42867381a7d5e205ec3bee6
speasley <s@stephenpeasley.com>
2023-04-03 11:09:24 -0600

    feat: use json data for dates an add main functionality

--- src/plugins/VueOccasions/index.js
+++ src/plugins/VueOccasions/index.js
@@ -1,14 +1,24 @@
 import * as core from "./core/index"
 import { name, version } from "../../../package.json"
-import occasions from './occasions.json' assert { type: 'json' }
+import occasionsData from './occasions.json' assert { type: 'json' }
 
 export default {
   install: (app, options) => {
     app.component("vue-occasions");
     const consolePre = `[${name}]`
     console.debug(`${consolePre} version ${ version } installed.`)
+    let occasions = { ...occasionsData }
+    // use options-supplied date, or today's date
     const todays_date = options && options.date ? core.todaysDate(options.date).slice(0,6) : core.todaysDate()
-    if (occasions[todays_date] !== null) {
+    // check for special date
+    Object.keys(occasions).forEach(key => {
+      if(key.slice(0, 1) === '_') {
+        // replace key with special-date result
+        occasions = core.renameKey(occasions, key, core.specialDate(key, todays_date));
+      }
+    });
+    // check for standard date
+    if (occasions[todays_date] !== undefined) {
       document.body.classList.add(occasions[todays_date])
       document.body.dataset.occasion = occasions[todays_date]
     }
