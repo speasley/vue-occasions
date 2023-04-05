835557f90c28e09afd43cb838df696505ca7c9ba
speasley <s@stephenpeasley.com>
2023-04-04 22:33:15 -0600

    refactor: weekdayBefore method

--- src/plugins/VueOccasions/core/weekday.js
+++ src/plugins/VueOccasions/core/weekday.js
@@ -100,7 +100,6 @@ const weekdayAfter = (parameters, override) => {
   target_day = Number(target_day);
   const year = new Date().getFullYear();
 
-  console.debug(`The ${target_weekday} after ${target_month} ${target_day} ${year} is May 01 2023`)
   if (override && override.length > 6) {
     year = override.slice(-4);
   }
@@ -132,39 +131,38 @@ const weekdayAfter = (parameters, override) => {
 
 const weekdayBefore = (parameters, override) => {
   const params = parameters.replace(/\s/g, '').split(','); // weekday, month, day
-  let [weekday, month, day] = params.map(param => param.trim());
-  weekday = weekdayIndex(weekday);
-  month = monthIndex(month);
-  day = Number(day);
-  const today = new Date();
-  const year = today.getFullYear();
+  let [target_weekday, target_month, target_day] = params.map(param => param.trim());
+  let target_weekday_index = weekdayIndex(target_weekday)
+  const target_month_index = monthIndex(target_month);
+  target_day = Number(target_day);
+  const year = new Date().getFullYear();
 
   if (override && override.length > 6) {
     year = override.slice(-4);
   }
   
-  let date;
-
-  if (day === weekdayName(weekday)) {
-    date = timestamp(month, day, year) - 604800; //minus seven days
+  let date = timestamp(target_month_index, target_day, year);
+  
+  let refDay = new Date(date * 1000).getDay();
+  if (refDay === target_weekday_index) {
+    date -= 604800; // subtract seven days if days of week match
   } else {
     let offset = 0;
-    let weekday_index = day; //weekday of the reference date, eg. 25
-
-    while (weekday_index !== weekday) {
-      weekday_index--;
+    while (refDay !== target_weekday_index) {
+      refDay--;
       offset++;
-      if (weekday_index === -1) {
-        weekday_index = 6; // when less than 0 (Sun), set to 6 (Sat)
+      if (refDay === -1) {
+        refDay = 6; // when less than 0 (Sun), set to 6 (Sat)
       }
     }
-    date = timestamp(month, day, year) - (86400 * offset);
+    date = timestamp(target_month_index, target_day, year) - (86400 * offset);
   }
+
   date = new Date(date * 1000);
   const formattedMonth = monthName(date.getMonth());
   const formattedDay = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-  
-  return `${formattedMonth} ${formattedDay}`;
+
+return `${formattedMonth} ${formattedDay}`;
 
 };
 
